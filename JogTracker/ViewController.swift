235d2ff73c78e6daf285ed36f586f1fc145a87b5
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-06-21 17:17:05 +0300

    Initial Commit

---
+++ JogTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JogTracker
+//
+//  Created by Александр on 6/21/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
