c30c8debe53a717e7b4755f97d2ef0c7ada70a2f
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-06-21 20:57:21 +0300

    JT-3: Provide base MVVM architecture

---
+++ JogTracker/Scenes/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  JogTracker
+//
+//  Created by Александр on 6/21/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import Foundation
+
+public class BaseViewController: UIViewController {
+    
+}
---
+++ JogTracker/Scenes/BaseViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewModel.swift
+//  JogTracker
+//
+//  Created by Александр on 6/21/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import UIKit
+
+public class BaseViewModel {
+    
+}
