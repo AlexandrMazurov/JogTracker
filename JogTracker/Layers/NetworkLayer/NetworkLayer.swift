459b5cfa0bc2dbf40dfe27434cd58e299f58d6f6
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-06-22 17:29:22 +0300

    JT-4: Setup Network Layer, provide errors handling

---
+++ JogTracker/Layers/NetworkLayer/NetworkLayer.swift
@@ -0,0 +1,65 @@
+//
+//  NetworkLayer.swift
+//  JogTracker
+//
+//  Created by Александр on 6/22/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import Moya
+
+enum NetworkError: LocalizedError {
+    case unknown
+    case invalidData
+    case invalidAuthentication
+    case notConnectedToInternet
+    
+    public var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "Unexpected error happened"
+        case .invalidData:
+            return "JSON model could not be recognized"
+        case .invalidAuthentication:
+            return "Provided invalid credentials"
+        case .notConnectedToInternet:
+            return "No Internet connection"
+        }
+    }
+}
+
+class NetwotkLayer {
+    
+    var authError = PublishSubject<NetworkError>()
+    
+    func sendRequest<T: Decodable, U: TargetType>(provider: MoyaProvider<U>, target: U) -> Single<T> {
+        return provider.rx
+            .request(target)
+            .map(T.self)
+            .catchError { [weak self] error in
+                guard let error = error as? MoyaError else {
+                    throw NetworkError.unknown
+                }
+                switch error {
+                case .jsonMapping, .objectMapping:
+                    throw NetworkError.invalidData
+                case .statusCode(_):
+                    self?.authError.onNext(.invalidAuthentication)
+                    throw NetworkError.invalidAuthentication
+                case .underlying(let error, _):
+                    self?.authError.onNext(.invalidAuthentication)
+                    let code = (error as NSError).code
+                    switch URLError.Code(rawValue: code) {
+                    case .notConnectedToInternet:
+                        throw NetworkError.notConnectedToInternet
+                    default: break
+                    }
+                    throw NetworkError.unknown
+                default:
+                    throw NetworkError.invalidData
+                }
+        }
+    }
+}
